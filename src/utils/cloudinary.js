c59828f28e7a0de9192fae28008d395913b92ec7
Abhijeet Chauhan <aschauhan1052002@gmail.com>
2024-02-12 19:01:03 +0530

    cloudinary upload fn setup

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,25 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from 'fs';
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET
+  });
+
+const uploadOnClodinary = async function name(localPath) {
+    
+    try {
+        if(!localPath) return null;
+        //Else proceed with file upload
+        const response  = await cloudinary.uploader.upload(localPath,{resource_type: auto})
+        console.log('File uploaded successfully',response.url)
+        return response
+    } catch (error) {
+        //Now if we encounter an error and were unable to upload the file to cloudinary then at least we can unlink the file in our server
+        fs.unlinkSync(localPath)
+        return null;
+    }
+}
+
+export {uploadOnClodinary}
