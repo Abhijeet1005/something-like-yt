f6e02addcfbd1437424c3802a2d797023005c2fd
Abhijeet Chauhan <aschauhan1052002@gmail.com>
2024-02-12 17:26:48 +0530

    pass check,access and refresh token method created

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 const userSchema = new  Schema({
 
@@ -45,5 +47,43 @@ const userSchema = new  Schema({
     }
 
 },{timestamps: true})
+
+userSchema.pre('save',async function(){      
+    if(!this.isModified("password")) return next();         //Here we are using normal function because we want the pre to have the context ('this') of the user schema
+    this.password = bcrypt.hash(this.password, 8)
+    next()
+})
+
+//This method check for the password entered 
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            username: this.username,
+            email: this.email,
+            fullname: this.fullname
+
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
 
 export const User = mongoose.model('User', userSchema)
